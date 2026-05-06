13aff7ed90783c0cb5a2e0db3c120ac67b4e3cec
Martin Flischman <26260699+martinflischman@users.noreply.github.com>
2026-05-06 12:38:55 +0200

    chore: accessibility and favicon improvements
    - add emoji favicon using svg data uri
    - remove empty id attribute from top flex div
    - add aria-label="draw two cards" to draw button
    - add alt text to dynamically generated card images using card value and suit

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function drawCards() {
       let cardsHtml = "";
 
       for (let card of data.cards) {
-        cardsHtml += `<img src="${card.image}" class="w-auto min-w-24 max-w-48">`;
+        cardsHtml += `<img src="${card.image}" alt="${card.value} of ${card.suit}" class="w-auto min-w-24 max-w-48">`;
       }
 
       if (data.remaining > 0) {
